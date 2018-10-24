dded4bc0bd778d259fd102288c8c676a825ea80a
CodeWebby <43757810+CodeWebby@users.noreply.github.com>
2018-10-23 19:47:14 -0500

    Exercise 1
    single wall turns run

---
+++ maze.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+	var wall =document.getElementById('boundary1');
+	    wall.addEventListener("mouseover", function() {
+	    	wall.style.background = "red";
+	    });
+}
