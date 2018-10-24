ec47ad410dee179c8b6bfdeb0cabb280ffa667b8
CodeWebby <43757810+CodeWebby@users.noreply.github.com>
2018-10-23 21:27:04 -0500

    Exercise 3
    alert when you hover over end

--- maze.js
+++ maze.js
@@ -1,8 +1,20 @@
 window.onload = function(){
 	var wall =document.querySelectorAll('.boundary');
+	var win = true;
+
 	  for (var i = 0; i < wall.length; i++){
 	    wall[i].addEventListener("mouseover", function() {
 	    	this.style.background = "red";
+	    	 win = false;
+	    	 return win;
 	    });
 	   } 
+
+	    end.addEventListener("mouseover", function() {
+         if (win == true) {
+          document.getElementById("status").innerHTML = "YOU WIN!";
+           alert("CONGRATULATIONS! YOU WIN!");
+          }
+          win = false;
+          });
 }
