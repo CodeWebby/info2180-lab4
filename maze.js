00b74ea169921106bde290aff60e21fa1ce93370
CodeWebby <43757810+CodeWebby@users.noreply.github.com>
2018-10-23 20:48:12 -0500

    Exercise 2
    All walls tutn red when mouse is over it

--- maze.js
+++ maze.js
@@ -1,6 +1,8 @@
 window.onload = function(){
-	var wall =document.getElementById('boundary1');
-	    wall.addEventListener("mouseover", function() {
-	    	wall.style.background = "red";
+	var wall =document.querySelectorAll('.boundary');
+	  for (var i = 0; i < wall.length; i++){
+	    wall[i].addEventListener("mouseover", function() {
+	    	this.style.background = "red";
 	    });
+	   } 
 }
